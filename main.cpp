5832ed758ce6a36e184236dbc6116c30881ab40b
Joe Gualtieri <gualtieri.joe@gmail.com>
2012-03-01 15:13:43 -0500

    working on playerclass choice

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+    string playerClass;
+    string playerName = "";
 
-    cout << "Hello world." << endl;
+    cout << "Hello, and welcome to Fight! To start, choose a class(Hunter, Warrior, Mage, or Rogue): ";
+    cin >> playerClass;
+    if(playerClass == "hunter" || playerClass == "Hunter" || playerClass == "warrior" || playerClass == "Warrior" || playerClass == "mage" || playerClass == "Mage" || playerClass == "rogue" || playerClass == "Rogue"){
+    playerClass[0] -= 32;
+    cout << "You chose to play as a " << playerClass << ". Now choose a name: ";
+    cin >> playerName;
+    }
+    else
+
+
+
 
     return 0;
 }
